84fe82304227b8428041915f6f395fb2964ecb53
Miraz Hossain <mirazshahin847@gmail.com>
2026-08-12 00:01:52 +0600

    Error fixed

--- src/Quizora.Web/obj/Debug/net9.0/Quizora.Web.AssemblyInfo.cs
+++ src/Quizora.Web/obj/Debug/net9.0/Quizora.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Quizora.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce480c2c61415361a80063845db539da7d20275d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef059b81bfbd8ad9d4effab5e66430374d58c786")]
 [assembly: System.Reflection.AssemblyProductAttribute("Quizora.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Quizora.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
