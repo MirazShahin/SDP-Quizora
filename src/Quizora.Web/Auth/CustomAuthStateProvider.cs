e710737ba3ec5fbc72052e01688943791076c3ab
Miraz Hossain <mirazshahin847@gmail.com>
2026-08-29 22:50:27 +0600

    Adding

--- src/Quizora.Web/Auth/CustomAuthStateProvider.cs
+++ src/Quizora.Web/Auth/CustomAuthStateProvider.cs
@@ -1,5 +1,4 @@
-﻿using Intersoft.Crosslight.Mobile;
-using Microsoft.AspNetCore.Components.Authorization;
+﻿using Microsoft.AspNetCore.Components.Authorization;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using Blazored.LocalStorage;
